3874757ac04e8dc1c646eb65cf1c1ef925ccd3ef
Chaithra S <chaithras3103@gmail.com>
2022-09-18 01:06:19 +0530

    first commit

---
+++ My-Portfolio/src/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./Home.css"
+class Home extends Component {
+    render(){
+        return(
+            <div className="home">
+                <h2>Hello, I am Chaithra S</h2>
+                <p>I am a programmer and developer.</p>
+            </div>
+        )
+    }
+}
+export default Home;
